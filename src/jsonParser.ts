b0eee2a8825bb22918b1fe130fb6df04fe00f1db
Сергей Токарeв <sat0304@yandex.ru>
2023-05-03 23:47:42 +0400

    movie parcer class is increased

--- src/jsonParser.ts
+++ src/jsonParser.ts
@@ -1,28 +1,82 @@
 
 export class MovieList {
   movieName: any;
+  movieOriginalName: any;
+  movieDescription: any;
+  
   actorList = [];
-  actorNames = [];
+  actorName = [];
+  actorLink = [];
+  actorKinopoiskId = [];
 
-//   createMovieName(movieList: any) {
-//     for (let i = 0; i < movieList.items.length; i++) {
-//         this.actors.kinopoisIid = movieList.items[i].id;
-//         this.actors.name = movieList.items[i].name;
-//         this.actors.url = movieList.items[i].price;
-//         this.actors[i] = this.movie;
-//     }
-//   }
-    async createMovieName(movieList: any) {
+  moviePoster: any;
+  movieTrailerLink: any;
+  movieYear: number;
+
+  movieLength: any;
+  movieAgeRating: any;
+
+  genreList = [];
+  genreName = [];
+  genreNameEng = [];
+
+  countryList = [];
+  countryName = [];
+  countryId = [];
+
+  movieRate: any;
+
+    async createMovieFeatures(movieList: any) {
         this.movieName = movieList.entityJSON.name;
-        return this.movieName;
+        this.moviePoster = movieList.entityJSON.poster;
+        this.movieOriginalName = movieList.entityJSON.originalName;
+        this.movieDescription = movieList.entityJSON.description;
+        this.movieTrailerLink = movieList.entityJSON.trailerLink;
+        this.movieYear = movieList.entityJSON.year;
+        this.movieLength = movieList.entityJSON.movieLength;
+        this.movieAgeRating = movieList.entityJSON.ageRating;
+        this.movieRate = movieList.entityJSON.rate.kinopoisk;
+        return  (
+          this.movieName,
+          this.moviePoster,
+          this.movieOriginalName,
+          this.movieDescription, 
+          this.movieTrailerLink,
+          this.movieYear,
+          this.movieLength,
+          this.movieAgeRating,
+          this.movieRate
+          );
     }
 
     async createActorList(movieList: any) {
       this.actorList = movieList.entityJSON.actors;
       for (let i = 0; i < this.actorList.length; i++) {
-        this.actorNames.push(this.actorList[i].name);
+        this.actorName.push(this.actorList[i].name);
+        this.actorLink.push(this.actorList[i].link);
+        this.actorKinopoiskId.push(this.actorList[i].kinopoiskId);
       }
-      return this.actorNames;
+      return (this.actorName, this.actorLink, this.actorKinopoiskId);
   }
 
+  async createGenreList(movieList: any) {
+    this.genreList = movieList.entityJSON.genres;
+    for (let i = 0; i < this.genreList.length; i++) {
+      this.genreName.push(this.genreList[i].genre);
+      this.genreNameEng.push(this.genreList[i].genreEng);
+      
+    }
+    return (this.genreName, this.genreNameEng);
+}
+
+
+async createCountryList(movieList: any) {
+  this.countryList = movieList.entityJSON.countries;
+  for (let i = 0; i < this.countryList.length; i++) {
+    this.countryName.push(this.countryList[i].country);
+    this.countryId.push(this.countryList[i].countryId);
+    
+  }
+  return (this.countryName, this.countryId);
+}
 }
--- src/messageHandler.ts
+++ src/messageHandler.ts
@@ -19,14 +19,30 @@ export default class MessageHandler{
         
         let response = {};
 
+
         const {genre, genreEng} = data;
         // const {entityJSON:{name, actors}} = data;
         const {id} = data;
-        const movieName = await movieList.createMovieName(data);
-        const actorNames = await movieList.createActorList(data);
+        await movieList.createMovieFeatures(data);
+        await movieList.createActorList(data);
+        await movieList.createGenreList(data);
+        await movieList.createCountryList(data);
 
-        console.log('the movie name from class is ', movieName);
-        console.log('the cast is ', actorNames);
+
+        console.log('the movie name from class is ', movieList.movieName);
+        console.log('the cast names are ', movieList.actorName);
+        console.log('the poster is ', movieList.moviePoster);
+        console.log('the movie Original Name is', movieList.movieOriginalName);
+        console.log('the movie description is', movieList.movieDescription);
+        console.log('the movie trailer link is', movieList.movieTrailerLink);
+        console.log('the movie release year is', movieList.movieYear);
+        console.log('the movie length is', movieList.movieLength);
+        console.log('the movie ageRating is', movieList.movieAgeRating);
+        console.log('the genres names are', movieList.genreName);
+        console.log('the genres names English are', movieList.genreNameEng);
+        console.log('the countries names are', movieList.countryName);
+        console.log('the countries ids are', movieList.countryId);
+        console.log('the kinopoisk rate is', movieList.movieRate);
 
         switch (routingKey) {
             case 'getGenres':
